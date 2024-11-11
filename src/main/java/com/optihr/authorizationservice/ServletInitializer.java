ae2b3d97b6344458eb7a1fc485aa43f96fb391c1
Alberto Abad <albertoabro@gmail.com>
2024-11-11 20:46:36 +0100

    initial commit

---
+++ src/main/java/com/optihr/authorizationservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.optihr.authorizationservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AuthorizationServiceApplication.class);
+	}
+
+}
